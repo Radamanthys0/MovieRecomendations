ef62559814cb08f828ce4c99ed8b62568c872109
Radamanthys <ludwigadamo@gmail.com>
2022-03-01 19:39:16 -0300

    clean

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -11,24 +11,24 @@ import {
 /**+
  * o Decorator Component irá definir algumas propriedades basicas do nosso componente, como por exemplo o html.
  */
-// @Component({
-//   selector: 'app-movie-card',
-//   templateUrl: './movie-card.component.html',
-//   styleUrls: ['./movie-card.component.scss'],
-// })
-// exemplo decorator
 @Component({
   selector: 'app-movie-card',
-  template: `<h1>HELLO WORLD</h1>`,
-  styles: [
-    `
-      h1 {
-        color: white;
-        text-decoration: underline;
-      }
-    `,
-  ],
+  templateUrl: './movie-card.component.html',
+  styleUrls: ['./movie-card.component.scss'],
 })
+// exemplo decorator
+// @Component({
+//   selector: 'app-movie-card',
+//   template: `<h1>HELLO WORLD</h1>`,
+//   styles: [
+//     `
+//       h1 {
+//         color: white;
+//         text-decoration: underline;
+//       }
+//     `,
+//   ],
+// })
 export class MovieCardComponent implements OnInit, OnDestroy {
   // para passar um dado de um componente pai, para um componente filho, utilizamos o @Input. veja mais no readme:
   @Input() movie: Movie | null = null;
